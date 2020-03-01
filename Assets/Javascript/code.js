fa5ee15aa3619cdae346a170af12c45334b29d62
Yfopla <ekblecker@gmail.com>
2020-03-01 15:59:32 -0500

    Added: updated CSS for time blocks

--- Assets/Javascript/code.js
+++ Assets/Javascript/code.js
@@ -7,21 +7,39 @@ currentDay.text(now)
 
 // Grabs the user input and stores it into the local storge
 // Each hour being given a unique ID
-$("#hour-9 .plan").val(localStorage.getItem("hour-9"));
-$("#hour-10 .plan").val(localStorage.getItem("hour-10"));
-$("#hour-11 .plan").val(localStorage.getItem("hour-11"));
-$("#hour-12 .plan").val(localStorage.getItem("hour-12"));
-$("#hour-13 .plan").val(localStorage.getItem("hour-13"));
-$("#hour-14 .plan").val(localStorage.getItem("hour-14"));
-$("#hour-15 .plan").val(localStorage.getItem("hour-15"));
-$("#hour-16 .plan").val(localStorage.getItem("hour-16"));
-$("#hour-17 .plan").val(localStorage.getItem("hour-17"));
-$("#hour-18 .plan").val(localStorage.getItem("hour-18"));
-$("#hour-19 .plan").val(localStorage.getItem("hour-19"));
+$("#hour-9 .col-md-10").val(localStorage.getItem("hour-9"));
+$("#hour-10 .col-md-10").val(localStorage.getItem("hour-10"));
+$("#hour-11 .col-md-10").val(localStorage.getItem("hour-11"));
+$("#hour-12 .col-md-10").val(localStorage.getItem("hour-12"));
+$("#hour-13 .col-md-10").val(localStorage.getItem("hour-13"));
+$("#hour-14 .col-md-10").val(localStorage.getItem("hour-14"));
+$("#hour-15 .col-md-10").val(localStorage.getItem("hour-15"));
+$("#hour-16 .col-md-10").val(localStorage.getItem("hour-16"));
+$("#hour-17 .col-md-10").val(localStorage.getItem("hour-17"));
 
 // When user hits save button, stores the text to local storage
 $(".saveBtn").on("click", function(){
-    let dayPlan = $(this).siblings(".plan").val();
+    let dayPlan = $(this).siblings(".col-md-10").val();
     let hourTime = $(this).parent().attr("id");
     localStorage.setItem(hourTime, dayPlan);
   });
+
+//Updates the time-blocks with the proper CSS
+function timeBlockColor (){
+    let time = moment().hours();
+    $('.row').each(function(){
+        let timeRow = parseInt($(this).attr("id").split("-")[1]);
+        if (timeRow < time) {
+            $(this).addClass("past")
+        } else if (timeRow === time) {
+            $(this).addClass("present")
+        } else {
+            $(this).addClass("future")
+        }
+    })
+
+
+}
+
+// Calls the function
+timeBlockColor ()
